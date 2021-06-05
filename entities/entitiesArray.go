66eedb232a3e5a0439d556b0224f19d71dc9ec22
JoseEvangelistaCucho <jzzthx@gmail.com>
2021-06-05 14:11:39 -0500

    Manejo de Errores

--- entities/entitiesArray.go
+++ entities/entitiesArray.go
@@ -5,13 +5,37 @@ import (
 )
 
 type ArrayEntities models.ArrayNum
+type ResponseVista models.ResponseVista
+type Response models.Response
 
-func (c *ArrayEntities) Rotate9() (a [][]int) {
-
+func (c *ArrayEntities) Rotate9() (r Response) {
+	var a [][]int
 	var m = c.Valor1
 	var n int = len(m)
 
-	a = make([][]int, n) // Crea la nueva "matriz"
+	for i := range m {
+		var TamArray int = len(m[i])
+
+		if TamArray != n {
+			if TamArray > n {
+				r.Code = 1
+				r.Message = "Cantidad de Digitos Mayor que Arrays"
+				return r
+			}
+			if TamArray < n {
+				r.Code = 2
+				r.Message = "Cantidad de Arrays Mayor que Digitos"
+				return r
+			}
+			r.Code = 3
+			r.Message = "Fatal Error"
+			return r
+
+		}
+
+	}
+
+	a = make([][]int, n) // Crea la nueva "matriz"///
 	for i := range a {
 		a[i] = make([]int, n) // y sus filas
 	}
@@ -21,5 +45,12 @@ func (c *ArrayEntities) Rotate9() (a [][]int) {
 			a[j][i] = m[i][n-j-1]
 		}
 	}
-	return
+
+	var vista models.ResponseVista
+	vista.ArrayVista = append(vista.ArrayVista, a...)
+	r.Code = 0
+	r.Message = "Respuesta Con Exito"
+	r.Response = vista
+
+	return r
 }
--- models/modelsArray.go
+++ models/modelsArray.go
@@ -3,3 +3,13 @@ package models
 type ArrayNum struct {
 	Valor1 [][]int `json:"valor1"`
 }
+
+type Response struct {
+	Code     int         `json:"code"`
+	Message  string      `json:"string"`
+	Response interface{} `json:"interface,omitempty"`
+}
+
+type ResponseVista struct {
+	ArrayVista [][]int `json:"arrayVista"`
+}
